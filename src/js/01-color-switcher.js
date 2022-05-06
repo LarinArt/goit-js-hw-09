17e5db049da4d1827037c27965ec53cf357b98e9
Larin Artem <larin_art@ukr.net>
2022-05-06 19:36:27 +0300

    task_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+    body: document.querySelector('body')
+}
+let timerId = null;
 
+refs.startBtn.disabled = false;
+
+refs.startBtn.addEventListener('click', () => {
+    timerId = setInterval(() => getRandomHexColor(), 1000);
+refs.startBtn.disabled = true;
+refs.stopBtn.disabled = false;})
+    
+refs.stopBtn.addEventListener('click', () => {
+    clearInterval(timerId);
+    refs.stopBtn.disabled = true;
+    refs.startBtn.disabled = false;
+});
+
+
+function getRandomHexColor() {
+  return refs.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
